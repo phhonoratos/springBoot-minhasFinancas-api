17bac0054a0562f377f6159fdfe9c6272989fe3d
Paulo Honorato <paulohonoratos@hotmail.com>
2021-08-12 17:53:24 -0300

    Correção da busca do saldo por status

--- src/main/java/com/honorato/minhasfinancas/model/repositories/LancamentoRepository.java
+++ src/main/java/com/honorato/minhasfinancas/model/repositories/LancamentoRepository.java
@@ -3,6 +3,7 @@ package com.honorato.minhasfinancas.model.repositories;
 import java.math.BigDecimal;
 
 import com.honorato.minhasfinancas.model.entities.Lancamento;
+import com.honorato.minhasfinancas.model.enums.StatusLancamento;
 import com.honorato.minhasfinancas.model.enums.TipoLancamento;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,7 +13,7 @@ import org.springframework.data.repository.query.Param;
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
 
     @Query(value = "select sum(l.valor) from Lancamento l join l.usuario u "
-                + "where u.id = :idUsuario and l.tipo =:tipo group by u")
-    BigDecimal obterSaldoPorTipoLancamentoEUsuario(@Param("idUsuario") Long idUsuario, @Param("tipo") TipoLancamento tipo);
+                + "where u.id = :idUsuario and l.tipo =:tipo and l.status =:status group by u")
+    BigDecimal obterSaldoPorTipoLancamentoEUsuarioEStatus(@Param("idUsuario") Long idUsuario, @Param("tipo") TipoLancamento tipo, @Param("status") StatusLancamento status);
     
 }
